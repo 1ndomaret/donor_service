857df4d4c79291c4b4ca8420277df1e4b1774bb8
Rey <syahfauzi73@rocketmail.com>
2026-09-07 22:02:17 +0700

    chore: todos

--- app/internal/router/router.go
+++ app/internal/router/router.go
@@ -14,4 +14,37 @@ func Register(e *echo.Echo,
 	private := api.Group("")
 	private.Use(echojwt.WithConfig(middleware.JwtConfig()), middleware.ParseJwtClaims)
 
+	/*
+		- Bikin blood-request
+			- dia ngecek juga donor profile yang sama blood-typenya, isAvailable, city
+				- kalau nemu dia akan bikin donor_matches
+
+		- Kalau nemu match, dia bisa nge invite user-user yang cocok
+
+		- Buat pendonor dia bisa accept/decline
+			- Auto decline setelah seminggu
+
+		- Kalau dia accept
+
+	*/
+
+	// TODOS:
+	// GOROUTINE buat cari matches jalan setiap jam
+
+	// POST /blood-requests
+	// GET  /blood-requests
+	// GET  /blood-requests/:id
+	// PUT  /blood-requests/:id/cancel
+
+	// GET /blood-requests/:id/matches
+
+	// POST /blood-requests/:id/invite/:donorId
+
+	// PATCH /donor-matches/:id/accept
+	// PATCH /donor-matches/:id/decline
+
+	// POST  /donations <-- yang bisa create si requester
+	// PATCH /donations/:id/complete <-- yang bisa complete si requester
+
+	// GET /hospitals
 }
