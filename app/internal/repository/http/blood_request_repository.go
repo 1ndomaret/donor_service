240f3354d23c76e75dbb3a27ff2490a65e97c2d7
Rey <syahfauzi73@rocketmail.com>
2026-09-08 20:23:48 +0700

    chore: code cleanup

--- app/internal/repository/http/blood_request_repository.go
+++ app/internal/repository/http/blood_request_repository.go
@@ -42,7 +42,10 @@ func (r *bloodRequestHttpRepo) SearchMatches(ctx context.Context, token string, 
 		queryParams["city"] = req.City
 	}
 
-	res, err := r.client.R().SetHeader("Authorization", token).SetContext(ctx).SetQueryParams(queryParams).SetResult(&apiRes).Get("/api/v1/users/donor-profile/search")
+	res, err := r.client.R().
+		SetHeader("Authorization", token).SetContext(ctx).SetQueryParams(queryParams).
+		SetResult(&apiRes).
+		Get("/api/v1/users/donor-profile/search")
 	if err != nil {
 		return nil, err
 	}
